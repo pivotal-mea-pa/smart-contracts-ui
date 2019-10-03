d0bdba4c241ab61b795bb92e766b6f30b4f6c1ad
hodeh <hodeh@pivotal.io>
2019-10-03 15:44:49 +0400

    dummy unit test

--- tests/smart-contracts-ui.js
+++ tests/smart-contracts-ui.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 describe('Basic Mocha String Test', function () {
  it(' unit test #1 of smart-contracts-ui', function () {
-        assert.equal("Hello".length, 4);
+        assert.equal("Hello".length, 5);
     });
  it('unit test #2 of smart-contracts-ui', function () {
         assert.equal("Hello".charAt(0), 'H');
